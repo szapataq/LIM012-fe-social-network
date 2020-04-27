e970d802ee9151caeac099c586b49a4184e4fde7
argierdfj <argierincon@gmail.com>
2020-04-26 21:20:57 -0500

    cambio de id y clase de msj de error y validando campos vacios

--- src/controller/register-controller.js
+++ src/controller/register-controller.js
@@ -5,39 +5,39 @@ const createUser = (email, password, names) => {
         displayName: names,
       });
 
-      const errorSignUp = document.querySelector('#errorSignUp');
+      const alertLogInSignUp = document.querySelector('#alertLogInSignUp');
 
       const configuration = {
         url: 'http://localhost:5501/src/',
       };
 
       res.user.sendEmailVerification(configuration).catch(() => {
-        errorSignUp.innerHTML = 'Ha ocurrido un error al crear la cuenta';
+        alertLogInSignUp.innerHTML = 'Ha ocurrido un error al crear la cuenta';
       });
 
       firebase.auth().signOut();
-
-      errorSignUp.innerHTML = 'Cuenta creada satisfactoriamente';
+      alertLogInSignUp.classList.add('alertSignUpOk');
+      alertLogInSignUp.innerHTML = 'Cuenta creada satisfactoriamente, se le ha enviado un correo para validar su cuenta.';
     })
     .catch((error) => {
       const errorCode = error.code;
       // const errorMessage = error.message;
-      const errorSignUp = document.querySelector('#errorSignUp');
+      const alertLogInSignUp = document.querySelector('#alertLogInSignUp');
       switch (errorCode) {
         case 'auth/email-already-in-use':
-          errorSignUp.innerHTML = 'Ya existe una cuenta con este correo';
+          alertLogInSignUp.innerHTML = 'Ya existe una cuenta con este correo';
           break;
         case 'auth/invalid-email':
-          errorSignUp.innerHTML = 'Ingrese un correo válido (por ejemplo alguien@example.com)';
+          alertLogInSignUp.innerHTML = 'Ingrese un correo válido (por ejemplo alguien@example.com)';
           break;
         case 'auth/operation-not-allowed':
-          errorSignUp.innerHTML = 'Comuníquese con el administrador';
+          alertLogInSignUp.innerHTML = 'Comuníquese con el Administrador';
           break;
         case 'auth/weak-password':
-          errorSignUp.innerHTML = 'La clave debe ser de mínimo 6 dígitos y al menos un número';
+          alertLogInSignUp.innerHTML = 'La clave debe ser de mínimo 6 dígitos';
           break;
         default:
-          errorSignUp.innerHTML = 'Ha ocurrido un error inesperado';
+          alertLogInSignUp.innerHTML = 'Ha ocurrido un error inesperado';
           break;
       }
     });
@@ -47,17 +47,30 @@ export const createAccount = () => {
   const varNameUser = document.querySelector('#names');
   const varEmailUser = document.querySelector('#email');
   const varPasswordUser = document.querySelector('#password');
+  const varTermConditions = document.querySelector('#termConditions');
   const nameUser = varNameUser.value;
   const emailUser = varEmailUser.value;
   const passwordUser = varPasswordUser.value;
-  createUser(emailUser, passwordUser, nameUser);
+  const termConditions = varTermConditions.checked;
+  const alertLogInSignUp = document.querySelector('#alertLogInSignUp');
+  alertLogInSignUp.classList.remove('alertSignUpOk');
+  if (nameUser === '') {
+    alertLogInSignUp.innerText = 'Debe ingresar su nombre';
+  } else if (emailUser === '') {
+    alertLogInSignUp.innerText = 'Debe ingresar su email';
+  } else if (passwordUser === '') {
+    alertLogInSignUp.innerText = 'Debe ingresar su contraseña';
+  } else if (!termConditions) {
+    alertLogInSignUp.innerText = 'Debe aceptar lo términos y condiciones';
+  } else {
+    createUser(emailUser, passwordUser, nameUser);
+  }
 };
 
 export const validateBtnSignUp = () => {
   const btnSignUp = document.querySelector('#btnSignUp');
   if (btnSignUp) {
     btnSignUp.addEventListener(('click'), () => {
-      console.log('creando cuenta');
       createAccount();
     });
   }

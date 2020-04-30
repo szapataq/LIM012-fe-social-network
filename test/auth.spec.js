a7d931705ad13ddd532fa7d2eec0c2bcde7cefc9
szapataq <szapata013@gmail.com>
2020-04-30 18:11:10 -0500

    actualizacion de test

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -17,15 +17,6 @@ global.firebase = firebasemock.MockFirebaseSdk(
   () => mockauth,
 );
 
-// test crear un nuevo usuario
-describe('Crear un usuario', () => {
-  it('Debería poder registrarse con email szapata013@gmail.com y password 12345678', () => createNewUser('szapata013@gmail.com', '12345678')
-    .then((user) => {
-      expect(user.email).toBe('szapata013@gmail.com');
-      expect(user.password).toBe('12345678');
-    }));
-});
-
 // funcion de firebase de iniciar sesion
 describe('Iniciar sesion', () => {
   it('Debería poder iniciar sesión', () => {
@@ -34,6 +25,15 @@ describe('Iniciar sesion', () => {
       expect(user.password).toBe('12345678');
     });
   });
+});
+
+// test crear un nuevo usuario
+describe('Crear un usuario', () => {
+  it('Debería poder registrarse con email szapata013@gmail.com y password 12345678', () => createNewUser('szapata013@gmail.com', '12345678')
+    .then((user) => {
+      expect(user.email).toBe('szapata013@gmail.com');
+      expect(user.password).toBe('12345678');
+    }));
 });
 
 // funcion de iniciar sesion con google

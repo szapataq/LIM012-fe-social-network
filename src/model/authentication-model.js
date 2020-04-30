c007bd2aba1f827e678bcf860794aed03bc02e58
argierdfj <argierincon@gmail.com>
2020-04-30 10:01:22 -0500

    organizando funciones de firebase

--- src/model/authentication-model.js
+++ src/model/authentication-model.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line max-len
+export const createNewUser = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password);
+
+// export const userCurrent = () => firebase.auth().currentUser;
+export const signOut = () => firebase.auth().signOut();
+
+// eslint-disable-next-line max-len
 export const signIn = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
 
 export const signInWithGoogle = () => {
@@ -10,7 +16,3 @@ export const signInWithFacebook = () => {
   const provider = new firebase.auth.FacebookAuthProvider();
   return firebase.auth().signInWithPopup(provider);
 };
-// eslint-disable-next-line max-len
-export const createNewUser = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password);
-// export const userCurrent = () => firebase.auth().currentUser;
-export const signOut = () => firebase.auth().signOut();

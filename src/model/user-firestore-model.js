2a4876a35f0b8fa09309403811920ebe490eb873
argierdfj <argierincon@gmail.com>
2020-05-01 17:10:39 -0500

    almacenamiento en BBDD de google y fb de un usuario

--- src/model/user-firestore-model.js
+++ src/model/user-firestore-model.js
@@ -11,9 +11,10 @@ export const createUserDB = (uid, email, coverPhoto, profilePicture, names, abou
     about,
   }).then((refDoc) => {
     console.log(`Id del usuario => ${refDoc.id}`);
-  }).catch((error) => {
-    console.log(error);
-  });
+  })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 // ? (READ) FUNCIÓN PARA LEER USUARIOS EN LA BASE DE DATOS
@@ -29,6 +30,29 @@ export const readUserDB = (uid) => {
         localStorage.setItem('userName', user.names);
         localStorage.setItem('userAbout', user.about);
       });
+    });
+};
+
+export const readCreateUserDB = (uid, email, coverPhoto, profilePicture, names, about) => {
+  const db = firebase.firestore();
+  return db.collection('users').where('uid', '==', uid)
+    .get()
+    .then((querySnapshot) => {
+      if (querySnapshot.empty) {
+        createUserDB(uid, email, coverPhoto, profilePicture, names, about);
+        localStorage.setItem('userCoverImg', coverPhoto);
+        localStorage.setItem('userProfileImg', profilePicture);
+        localStorage.setItem('userName', names);
+        localStorage.setItem('userAbout', about);
+      } else {
+        querySnapshot.forEach((refDoc) => {
+          const user = refDoc.data();
+          localStorage.setItem('userCoverImg', user.coverPhoto);
+          localStorage.setItem('userProfileImg', user.profilePicture);
+          localStorage.setItem('userName', user.names);
+          localStorage.setItem('userAbout', user.about);
+        });
+      }
     });
 };
 

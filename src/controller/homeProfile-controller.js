262bbc63bb447f4ad6aa7e0090b03962fd130fc5
argierdfj <argierincon@gmail.com>
2020-05-02 23:29:41 -0500

    añadiendo animación para likes

--- src/controller/homeProfile-controller.js
+++ src/controller/homeProfile-controller.js
@@ -12,3 +12,31 @@ export const signOutUser = () => {
       window.location.hash = '#/login';
     });
 };
+
+export const btnLikes = () => {
+  const interval = setInterval(() => {
+    const svgIcons = document.querySelectorAll('.iconLike');
+    if (svgIcons.length) {
+      clearInterval(interval);
+      svgIcons.forEach((svgIcon) => {
+        const burst = new mojs.Burst({
+          count: 20,
+          left: (svgIcon.getBoundingClientRect().left + (svgIcon.clientWidth / 1.4)),
+          top: (svgIcon.getBoundingClientRect().top + (svgIcon.clientHeight / 1.4)),
+          children: {
+            shape: ['circle', 'polygon', 'rect'],
+            fill: ['#6886c5', '#ffe0ac', '#ffacb7'],
+            degreeShift: 'rand(-360, 360)',
+            delay: 'stagger(0,30)',
+          },
+          duration: 400,
+        });
+
+        svgIcon.addEventListener('click', (e) => {
+          e.currentTarget.classList.toggle('svg-filled');
+          burst.replay();
+        });
+      });
+    }
+  }, 1000);
+};

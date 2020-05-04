ec5d03b9dce867173d9bb6322e4a80b988e6eba2
argierdfj <argierincon@gmail.com>
2020-05-03 19:00:24 -0500

    moviendo funcion de storage a su propio archivo js

--- src/model/posts-firestore-model.js
+++ src/model/posts-firestore-model.js
@@ -99,30 +99,4 @@ export const readPostDB = () => {
     });
 };
 
-export const shareImgPost = (file, uid) => {
-  const refStorage = firebase.storage().ref(`imgPosts/${uid}/${file.name}`);
-  const taskStorage = refStorage.put(file);
-
-  const stateSnapshot = (snapshot) => {
-    const percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-    console.log(percent);
-    // $('.determinate').attr('style', `width: ${percent}%`);
-  };
-
-  const catchError = (err) => { console.log(err.message); };
-
-  const fileReady = () => {
-    taskStorage.snapshot.ref.getDownloadURL()
-      .then((url) => {
-        console.log(url);
-        sessionStorage.setItem('imgNewPost', url);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-
-  taskStorage.on('state_changed', stateSnapshot, catchError, fileReady);
-};
-
 export const deleteNote = idpost => firebase.firestore().collection('posts').doc(idpost).delete();

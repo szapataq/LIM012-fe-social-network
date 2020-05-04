3620fd131546aa06851a6540f5e2a827d3150afb
argierdfj <argierincon@gmail.com>
2020-05-04 18:00:03 -0500

    fn para publicar en post home lista

--- src/controller/post-controller.js
+++ src/controller/post-controller.js
@@ -11,16 +11,30 @@ import {
   deletePostsOnClick,
 } from './homeProfile-controller.js';
 
-// const priv = document.querySelector('#private');
-// const pub = document.querySelector('public');
+const priv = document.querySelector('#private');
+const pub = document.querySelector('#public');
+
+if (priv) {
+  pub.addEventListener('click', () => {
+    sessionStorage.setItem('privacy', 1);
+  });
+}
+
+if (pub) {
+  priv.addEventListener('click', () => {
+    sessionStorage.setItem('privacy', 2);
+  });
+}
+
 
 export const createNewPost = (post, privacyPostArea) => {
   const uid = firebase.auth().currentUser.uid;
   const names = localStorage.getItem('userName');
   const profilePic = localStorage.getItem('userProfileImg');
   const photo = sessionStorage.getItem('imgNewPost');
+  const privacyPost = sessionStorage.getItem('privacy') || privacyPostArea || 1;
 
-  createPostDB(uid, names, profilePic, post, photo, privacyPostArea)
+  createPostDB(uid, names, profilePic, post, photo, privacyPost)
     .then((docRef) => {
       sessionStorage.removeItem('imgNewPost');
       console.log('Document written with ID: ', docRef.id);

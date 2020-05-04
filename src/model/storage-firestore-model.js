d33bf764ba2776fa4ffa679b79f7af45255ae94c
szapataq <szapata013@gmail.com>
2020-05-03 21:08:22 -0500

    recueracion de archivo

---
+++ src/model/storage-firestore-model.js
@@ -0,0 +1,27 @@
+export const shareImgPost = (file, uid) => {
+  const refStorage = firebase.storage().ref(`imgPosts/${uid}/${file.name}`);
+  const taskStorage = refStorage.put(file);
+
+  const stateSnapshot = (snapshot) => {
+    const percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+    console.log(percent);
+    // $('.determinate').attr('style', `width: ${percent}%`);
+  };
+
+  const catchError = (err) => {
+    console.log(err.message);
+  };
+
+  const fileReady = () => {
+    taskStorage.snapshot.ref.getDownloadURL()
+      .then((url) => {
+        console.log(url);
+        sessionStorage.setItem('imgNewPost', url);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  taskStorage.on('state_changed', stateSnapshot, catchError, fileReady);
+};

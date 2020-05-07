361bacb259ae2f8d28d3d20ea66dad9febf886b8
szapataq <szapata013@gmail.com>
2020-05-06 20:03:32 -0500

    pequeño cambio

--- src/controller/post-controller.js
+++ src/controller/post-controller.js
@@ -30,21 +30,16 @@ export const createNewPost = (post, privacyPostArea) => {
 };
 
 export const readingPosts = (querySnapshot) => {
-  const uid = firebase.auth().currentUser.uid;
-  // console.log('uid del user', uid);
-  let postList = '';
   const container = document.querySelector('.container-new-post');
   if (querySnapshot.empty) {
     container.innerHTML = notYetPost;
   } else {
     const uid = firebase.auth().currentUser.uid;
-    // console.log('uid del user', uid);
     let postList = '';
     querySnapshot.forEach((refDoc) => {
       const post = refDoc.data();
       postList += templatePost(post.profilePicture, post.names, post.date,
         post.post, post.photo, post.likes, post.comments, refDoc.id, uid, post.uid);
-      // console.log(refDoc.uid);
       container.innerHTML = postList;
       return postList;
     });

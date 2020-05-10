388f97d2e7f4d738baca13dbc378fe2da6974723
argierdfj <argierincon@gmail.com>
2020-05-10 15:35:29 -0500

    inhabilitano icono de enviar comentario sin contenido

--- src/view/templateHomeProfile.js
+++ src/view/templateHomeProfile.js
@@ -196,9 +196,6 @@ export const templatePost = (photoUrl, names, privacy, date, textPost,
         <input type="text" placeholder="Agrega un comentario..." class="inputComment">
         <svg aria-label="Compartir publicación" class="icon-send _8-yf5 " fill="#b1b1b1" height="24" viewBox="0 0 48 48" width="24"><path d="M46.5 3.5h-45C.6 3.5.2 4.6.8 5.2l16 15.8 5.5 22.8c.2.9 1.4 1 1.8.3L47.4 5c.4-.7-.1-1.5-.9-1.5zm-40.1 3h33.5L19.1 18c-.4.2-.9.1-1.2-.2L6.4 6.5zm17.7 31.8l-4-16.6c-.1-.4.1-.9.5-1.1L41.5 9 24.1 38.3z"></path><path d="M14.7 48.4l2.9-.7"></path></svg>
         </div>
-        <div id="emptyComment" class="hide emptyPost empCom">
-          <p></p>
-        </div>
     </div>
     <div id="containerComment-${id}" class="hide container-comments"></div>`;
 
@@ -222,18 +219,11 @@ export const templatePost = (photoUrl, names, privacy, date, textPost,
     iconSendComment.addEventListener('click', () => {
       const commentValue = varComment.value.trim();
       if (!commentValue) {
-        const inputComment = eachPost.querySelector('.new-comment');
-        const emptyComment = eachPost.querySelector('#emptyComment');
-        inputComment.classList.add('noBorder');
-        emptyComment.classList.remove('hide');
-        emptyComment.innerText = '👀 Parece que tu comentario está vacío. 👆';
-        setTimeout(() => {
-          inputComment.classList.remove('noBorder');
-          emptyComment.classList.add('hide');
-        }, 1000);
+      // iconSendComment.setAttribute('disabled', true);
       } else {
         createNewComment(id, commentValue);
         varComment.value = '';
+        iconSendComment.classList.remove('activeSend');
       }
     });
   }

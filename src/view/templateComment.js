85519abd2f796c0ebe6d6da725492c579db018d7
szapataq <szapata013@gmail.com>
2020-05-11 22:42:22 -0500

    Plantilla de Texto de Comentario
    Se creo la funcion template comentario la cual llama a los objetos de cada comentario creado

--- src/view/templateComment.js
+++ src/view/templateComment.js
@@ -1,4 +1,5 @@
-export const templateComment = (names, profilePicture, comment, date, idComment) => {
+export const templateComment = (names, profilePicture, comment,
+  date, idComment) => {
   const containerComments = document.createElement('div');
   containerComments.className = 'name-comment';
 
@@ -7,22 +8,31 @@ export const templateComment = (names, profilePicture, comment, date, idComment)
      <div class="info-user-comment">
        <h4 class="nameUser">${names}</h4>
        <p class="dateComment">${date}</p>
-       <p class="p-comment">${comment}</p>
+       <div class="text-post">
+          <p class="p-comment" id="textComment-${idComment}">${comment}</p>
+          <div class="save-comment hide" idcomment="${idComment}"><i class="far fa-save"></i></div>
+       </div>
      </div>
      <spam class="comment">
       <i id="options-${idComment}" class="fas fa-ellipsis-h"></i>
      </spam>
      <div class="tooltip-container hide" id="show-toolTip-${idComment}">
-      <div class="arrow"></div>
+        <div class="arrow"></div>
         <div class="tooltip">
-          <div class="opt"> <i class="fas fa-edit icon-tool"></i> <span>Editar</span></div>
+          <div idcomment="${idComment}" class="opt update-comment edit-${idComment}"> <i class="fas fa-edit icon-tool"></i> <span>Editar</span></div>
           <div idcomment="${idComment}" class="opt del"> <i class="fas fa-trash-alt icon-tool"></i><span>Eliminar</span></div>
         </div>
-      </div>`;
+     </div>`;
 
   containerComments.innerHTML = template;
   const editDelete = containerComments.querySelector(`#options-${idComment}`);
+
   editDelete.addEventListener(('click'), () => {
+    const toolContainer = document.querySelector(`#show-toolTip-${idComment}`);
+    toolContainer.classList.toggle('hide');
+  });
+  const editComment = containerComments.querySelector(`.edit-${idComment}`);
+  editComment.addEventListener(('click'), () => {
     const toolContainer = document.querySelector(`#show-toolTip-${idComment}`);
     toolContainer.classList.toggle('hide');
   });

464b9a7fe4d6cf0e996ddc1285de58a5d059343f
szapataq <szapata013@gmail.com>
2020-05-18 22:41:18 -0500

    adicion de evento de volver atras

--- src/view/error.js
+++ src/view/error.js
@@ -5,13 +5,18 @@ export default () => {
         <img src="./img/error.png" alt="" class="img-error">
       </figure>
       <div class="body-error">
-        <h2>Oops!, page not found...</h4>
-        <P>Page doesn't exist or some other error occured. Go to our home page or go back to previous page.</P>
+        <h2>Oops!, Pagina no encontrada...</h4>
+        <P>La página no existe o se produjo algún otro error. Vaya a nuestra página de inicio o regrese a la <span class="back">página anterior</span>.</P>
       </div>
     </div>`;
 
   const sectionError = document.createElement('section');
   sectionError.innerHTML = viewDifferent;
+
+  const back = sectionError.querySelector('.back');
+  back.addEventListener('click', () => {
+    window.history.back();
+  });
 
   return sectionError;
 };
